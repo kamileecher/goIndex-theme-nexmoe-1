b90da5672bc0a420437e43ebc8169b0cafb9d12a
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-08-10 20:31:29 +0300

    Update app.js

--- dist/themes/light/app.js
+++ dist/themes/light/app.js
@@ -276,7 +276,7 @@ function list(path) {
     <ul class="mdui-list"> 
      <li class="mdui-list-item th"> 
       <div class="mdui-col-xs-12 mdui-col-sm-7">
-       文件
+       File
   <i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="more">expand_more</i>
       </div> 
       <div class="mdui-col-sm-3 mdui-text-right">
